9223434101b28bcefe65c15bc0da6cffb373ee71
Vladimir Lefterov <https://github.com/hanceanic>
2024-04-24 00:25:21 +0300

    Adding proxyDecorator property to ReorderableGridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:reorderable_grid/reorderable_grid.dart';
 
@@ -14,24 +16,25 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: BoxHolder(),
+      home: const BoxHolder(),
     );
   }
 }
 
-class ShoppingCartButton extends StatefulWidget {
-  const ShoppingCartButton({super.key});
+class AnimatedBox extends StatefulWidget {
+  const AnimatedBox({super.key});
 
   @override
-  State<ShoppingCartButton> createState() => _ShoppingCartButtonState();
+  State<AnimatedBox> createState() => _AnimatedBoxState();
 }
 
-class _ShoppingCartButtonState extends State<ShoppingCartButton> {
+class _AnimatedBoxState extends State<AnimatedBox> {
   bool isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       body: Center(
         child: GestureDetector(
           onTap: () {
@@ -47,15 +50,11 @@ class _ShoppingCartButtonState extends State<ShoppingCartButton> {
               color: isExpanded ? Colors.blue : Colors.red,
               borderRadius: BorderRadius.circular(10),
               border: Border.all(width: 3.0),
-            ), // BoxDecoration
-//             child: const Icon(
-//               Icons.shopping_cart,
-//               color: Colors.white,
-//             ), // Icon
-          ), // AnimatedContainer
-        ), // GestureDetector
-      ), // Center
-    ); // Scaffold
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -69,6 +68,38 @@ class BoxHolder extends StatefulWidget {
 class _BoxHolderState extends State<BoxHolder> {
   final items = List<int>.generate(25, (index) => index);
 
+  // Widget proxyDecorator(Widget child, int index, Animation<double> animation) {
+  //   return AnimatedBuilder(
+  //     animation: animation,
+  //     builder: (BuildContext context, Widget? child) {
+  //       return Material(
+  //         elevation: 0,
+  //         color: Colors.transparent,
+  //         child: child,
+  //       );
+  //     },
+  //     child: child,
+  //   );
+  // }
+
+  Widget proxyDecorator(Widget child, int index, Animation<double> animation) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (BuildContext context, Widget? child) {
+        final double animValue = Curves.easeInOut.transform(animation.value);
+        // final double elevation = lerpDouble(1, 6, animValue)!;
+        final double scale = lerpDouble(1, 1.02, animValue)!;
+        return Transform.scale(
+          scale: scale,
+          // Create a Card based on the color and the content of the dragged one
+          // and set its elevation to the animated value.
+          child: child,
+        );
+      },
+      child: child,
+    );
+  }
+
   void _onReorder(int oldIndex, int newIndex) {
     setState(() {
       final item = items.removeAt(oldIndex);
@@ -81,21 +112,14 @@ class _BoxHolderState extends State<BoxHolder> {
     return MaterialApp(
       home: Scaffold(
         body: ReorderableGridView.extent(
+          proxyDecorator: proxyDecorator,
           maxCrossAxisExtent: 250,
           onReorder: _onReorder,
           childAspectRatio: 1,
           children: items.map((item) {
-            /// map every list entry to a widget and assure every child has a
-            /// unique key
-            return ShoppingCartButton(
+            return AnimatedBox(
               key: ValueKey(item),
             );
-            // return Card(
-            //   key: ValueKey(item),
-            //   child: Center(
-            //     child: Text(item.toString()),
-            //   ),
-            // );
           }).toList(),
         ),
       ),
